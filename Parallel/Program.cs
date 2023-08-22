6ae798caea31b087254691bca47685a9bc32d571
Alan Ortiz <suza.ortiz4@gmail.com>
2023-08-22 13:31:52 -0700

    Aquí terminamos el proyecto, se hizo un cambio al nombre del proyecto por lo menos en el namespace porque me causo una situacion donde no podria utilizar el parallel foreach por ese nombre (problema resuelto), y se agregaron las funciones de medicion de tiempo todo en milisegundos. PD: se agrego la carpeta de imagenes a la carpeta del proyecto.

--- Parallel/Program.cs
+++ Parallel/Program.cs
@@ -1,15 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
-namespace Parallel
+namespace pruebaParallel
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void ProcesarImagen(string ruta)
         {
+            // Ejemplo: calcular el MD5 del archivo
+            using (var md5 = MD5.Create())
+            {
+                using (var datos = File.OpenRead(ruta))
+                {
+                    byte[] hash = md5.ComputeHash(datos);
+                    string hashString = BitConverter.ToString(hash).Replace("-", "").ToLower();
+                    //Console.WriteLine($"Archivo: {Path.GetFileName(ruta)}, MD5: {hashString}"); Eso fue para pruebas  nomas
+                }
+            }
+        }
+        public static void Main(string[] args)
+        {
+            string rutaCarpeta = @"C:\Users\sumio\OneDrive\Escritorio\Unikino\UK-ArquiFinal\Wallpapers 2020"; // Reemplaza con la ruta de tu carpeta
+            string[] archivos = Directory.GetFiles(rutaCarpeta);
+
+            // Procesamiento síncrono
+            Stopwatch syncSW = new Stopwatch();
+            syncSW.Start();
+            foreach (string archivo in archivos)
+            {
+                ProcesarImagen(archivo);
+            }
+            syncSW.Stop();
+
+            // Procesamiento en paralelo con Parallel.ForEach
+            Stopwatch parallelSW = new Stopwatch();
+            parallelSW.Start();
+
+            Parallel.ForEach(archivos, archivo =>
+            {
+                ProcesarImagen(archivo);
+            });
+            parallelSW.Stop();
+
+            Console.WriteLine("\nTiempo de procesamiento síncrono: " + syncSW.Elapsed.TotalMilliseconds + " milisegundos");
+            Console.WriteLine("Tiempo de procesamiento en paralelo: " + parallelSW.Elapsed.TotalMilliseconds + " milisegundos");
+
+            double porcentajeDeMejora = (syncSW.Elapsed.TotalMilliseconds - parallelSW.Elapsed.TotalMilliseconds) / syncSW.Elapsed.TotalMilliseconds * 100;
+            Console.WriteLine("La mejora del metodo paralelo es " + (int)porcentajeDeMejora + "% mas rapido con respecto al metodo sincrono.");
+
+
+            Console.ReadLine();
         }
     }
 }
